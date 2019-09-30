00e0018693cb2493dc42e8d791a195aa7f4eb2a3
Mithun1077 <mithunias10@gmail.com>
2019-09-30 12:53:39 +0530

    java final assement

--- UST JFS(MITHUN R)/src/com/dev/employee2/Emp12.java
+++
@@ -1,9 +0,0 @@
-package com.dev.employee2;
-
-public interface Emp12 {
-	public void add(int key,Emp11 value);
-	public void delete(int key);
-	public void update(int key,Emp11 value);
-	public void retrive(int key);
-
-}
